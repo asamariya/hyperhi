ccdb51ec169f04a0bbc76dddee2e8d84a0648b09
Abhishek Samariya <asamariya@gmail.com>
2019-12-17 10:35:17 +1100

    Made the cursor follow the mouse

--- hyperhi.js
+++ hyperhi.js
@@ -8,10 +8,19 @@ const shrinkCursor = () => {
   cursor.classList.remove('is-down');
 };
 
+const moveCursor = (x, y) => {
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+};
+
 document.addEventListener('mousedown', () => {
   growCursor();
 });
 
 document.addEventListener('mouseup', () => {
   shrinkCursor();
+});
+
+document.addEventListener('mousemove', e => {
+  moveCursor(e.pageX, e.pageY);
 });
